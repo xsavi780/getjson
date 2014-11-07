4b6c9a62fdbf1330847855a52ac8222a558e898d
Chris Whong <chris.whong@socrata.com>
2014-11-06 21:12:33 -0500

    Cleaned up the code

--- js/script.js
+++ js/script.js
@@ -1,39 +1,49 @@
-var map = L.map('map').setView([40.68025, -74.00155], 13);
 
+//set up our map
+var map = L.map('map')
+	.setView([40.68025, -74.00155], 13);
+
+
+//set up basemap tiles from mapbox
 L.tileLayer('http://{s}.tiles.mapbox.com/v3/examples.map-i875mjb7/{z}/{x}/{y}.png', {
     maxZoom: 18
 }).addTo(map);
 
-var marker = L.marker([40.68025, -74.00155]).addTo(map);
-marker.bindPopup("Larry's house").openPopup();
 
-function makeMarkers(feature, layer) {
-	console.log(feature);
-	layer.bindPopup(
-			feature.properties.Address
-			+ " <br/> The Zoning is " 
-			+ feature.properties.AllZoning1 
-			+ "<div class = '" 
-			+ feature.properties.BBL 
-			+ "'>Now I am classed "
-			+ feature.properties.BBL 
-			+ "</div>"
-	  );
-
-	$('body').append("<div>" + feature.properties.BBL + "</div>")
-
-}
-
-
-
+//load external geoJSON
 $.getJSON('centroids.geojson',function(data){
-	//console.log(data);
-
 	L.geoJson(data.features, {
     onEachFeature: makeMarkers
   }).addTo(map);
-
-
-
-
 });
+
+
+function makeMarkers(feature, layer) {
+	var thisFeature = feature.properties;
+
+	//bind a leaflet popup to the marker
+	layer.bindPopup(
+			thisFeature.Address
+			+ " <br/> The Zoning is " 
+			+ thisFeature.AllZoning1 
+			+ "<div class = '" 
+			+ thisFeature.BBL 
+			+ "'>Now I am classed "
+			+ thisFeature.BBL 
+			+ "</div>"
+	  );
+
+	//set up a bunch of divs classed using the BBL to see if we can select them later
+	$('body').append(
+		"<div class = '"
+		+ thisFeature.BBL
+		+ "'>" 
+		+ thisFeature.BBL + "</div>")
+
+	//attach a click listener to every feature
+	//that selects divs classed with the current marker's BBL
+	layer.on("click",function(e){
+		$('.' + thisFeature.BBL)
+			.css('background','steelblue');
+	})
+}
