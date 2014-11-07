e5726a1d56a83b011b5de4b59fb956176db6e746
Chris Whong <chris.whong@socrata.com>
2014-11-06 20:55:49 -0500

    First Commit

--- js/script.js
+++ js/script.js
@@ -6,3 +6,34 @@ L.tileLayer('http://{s}.tiles.mapbox.com/v3/examples.map-i875mjb7/{z}/{x}/{y}.pn
 
 var marker = L.marker([40.68025, -74.00155]).addTo(map);
 marker.bindPopup("Larry's house").openPopup();
+
+function makeMarkers(feature, layer) {
+	console.log(feature);
+	layer.bindPopup(
+			feature.properties.Address
+			+ " <br/> The Zoning is " 
+			+ feature.properties.AllZoning1 
+			+ "<div class = '" 
+			+ feature.properties.BBL 
+			+ "'>Now I am classed "
+			+ feature.properties.BBL 
+			+ "</div>"
+	  );
+
+	$('body').append("<div>" + feature.properties.BBL + "</div>")
+
+}
+
+
+
+$.getJSON('centroids.geojson',function(data){
+	//console.log(data);
+
+	L.geoJson(data.features, {
+    onEachFeature: makeMarkers
+  }).addTo(map);
+
+
+
+
+});
