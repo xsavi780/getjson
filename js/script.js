19ef0579d59b3f8d8c58acca550582d4ef8d45d3
Chris Whong <chris.whong@socrata.com>
2014-11-07 13:28:45 -0500

    Added lots of interactivity with the geoJSON layer

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 
 //set up our map
 var map = L.map('map')
-	.setView([40.68025, -74.00155], 13);
+	.setView([40.78, -73.9], 12);
 
 
 //set up basemap tiles from mapbox
@@ -12,10 +12,65 @@ L.tileLayer('http://{s}.tiles.mapbox.com/v3/examples.map-i875mjb7/{z}/{x}/{y}.pn
 
 //load external geoJSON
 $.getJSON('centroids.geojson',function(data){
-	L.geoJson(data.features, {
-    onEachFeature: makeMarkers
+	var geojsonLayer = L.geoJson(data.features, {
+    onEachFeature: makeMarkers,
+    pointToLayer: function (feature, latlng) {
+				return L.circleMarker(latlng, {
+					radius: 8,
+					fillColor: "#ff7800",
+					color: "#000",
+					weight: 1,
+					opacity: 1,
+					fillOpacity: 0.8
+				});
+			}
   }).addTo(map);
+
+	$('.sideBarItem')
+	.mouseenter(function(){
+		$(this).toggleClass('highlight');
+		var thisBBL = $(this).attr('id');
+		highlightMarker(geojsonLayer,thisBBL);
+	})
+	.mouseout(function(){
+		$(this).toggleClass('highlight');
+		//highlightMarker(geojsonLayer,thisBBL);
+	})
+	.click(function(){
+		var clicked = $(this);
+		geojsonLayer.eachLayer(function(marker) {
+			if(clicked.attr('id')==marker.feature.properties.BBL) {
+				console.log(marker._latlng);
+				map.panTo(marker._latlng);
+				marker.openPopup();
+			}
+		})
+	});
+
+
+
+
+
 });
+
+
+function highlightMarker(geojsonLayer,thisBBL) {
+  geojsonLayer.eachLayer(function(marker) {
+		if(thisBBL==marker.feature.properties.BBL) {
+   		marker.setStyle({
+   			fillColor:'#fff',
+   			radius: 12
+   		})
+   		console.log(marker.options.fillColor);
+		} else {
+			marker.setStyle({
+				fillColor:'#ff7800',
+				radius:8
+			});
+		}
+  });
+}
+
 
 
 function makeMarkers(feature, layer) {
@@ -24,21 +79,16 @@ function makeMarkers(feature, layer) {
 	//bind a leaflet popup to the marker
 	layer.bindPopup(
 			thisFeature.Address
-			+ " <br/> The Zoning is " 
-			+ thisFeature.AllZoning1 
-			+ "<div class = '" 
-			+ thisFeature.BBL 
-			+ "'>Now I am classed "
-			+ thisFeature.BBL 
-			+ "</div>"
+			+ " <br/> Zoned " 
+			+ thisFeature.AllZoning1
 	  );
 
 	//set up a bunch of divs classed using the BBL to see if we can select them later
-	$('body').append(
-		"<div class = '"
+	$('#sideBar').append(
+		"<div class = 'sideBarItem' id='"
 		+ thisFeature.BBL
 		+ "'>" 
-		+ thisFeature.BBL + "</div>")
+		+ thisFeature.Address.toLowerCase() + "</div>")
 
 	//attach a click listener to every feature
 	//that selects divs classed with the current marker's BBL
@@ -47,3 +97,5 @@ function makeMarkers(feature, layer) {
 			.css('background','steelblue');
 	})
 }
+
+
